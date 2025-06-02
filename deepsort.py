403609772bbca1c777a692b5108f581bcb4c37ee
Mario Chamdjoko Jati Mulyo <mario@digits.id>
2025-06-03 00:52:32 +0700

    refactor(DeepSort): add print for debugging
    Add print for debugging information to find fast-reid directory

--- deepsort.py
+++ deepsort.py
@@ -8,6 +8,7 @@ import json
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'thirdparty/fast-reid'))
+print("Using fast-reid from {}".format(os.path.join(os.path.dirname(__file__), 'thirdparty/fast-reid')))
 
 from detector import build_detector
 from deep_sort import build_tracker
